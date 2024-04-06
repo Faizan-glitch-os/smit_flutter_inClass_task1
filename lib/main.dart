bcc2abf0439d518650acd22cd33a951aad817d50
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-04-06 15:58:16 +0500

    Practising on Incrementing and Decrementing values on Screen using Elevated Buttons, also a Floating Action Button to reload the numbers back to zero

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home'),
     );
   }
 }
